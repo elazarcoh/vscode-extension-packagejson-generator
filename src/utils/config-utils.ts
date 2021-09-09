48bea69abf1fe85ae85b8184a94548412cc0c4bc
Elazar <elazarcoh@gmail.com>
2021-09-09 17:03:31 +0300

    bugfix config getter

--- src/utils/config-utils.ts
+++ src/utils/config-utils.ts
@@ -3,9 +3,9 @@ import { workspace } from 'vscode';
 export function ConfigurationGetter<C extends { [key: string]: any }>(
   section: string
 ) {
-  const config = workspace.getConfiguration(section);
   return <K extends keyof C>(subsection: K) => {
-    // @ts-expect-error subsection is always of type string, even though the compiler doesn't see it
+    const config = workspace.getConfiguration(section);
+    // @ts-expect-error subsection is always of type string, though the compiler doesn't see it
     return config.get<C[K]>(subsection);
   };
 }
